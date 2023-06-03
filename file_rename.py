da73918bd23dd22731750a8402e399aef16bef45
LeeHyungSeop <lhhss0927@gmail.com>
2023-06-03 21:19:36 +0900

    first python3 /Users/ihyeongseob/Desktop/인천대/3학년/1학기/데이터사이언스/file_rename.py

---
+++ file_rename.py
@@ -0,0 +1,14 @@
+import os
+
+def rename_files():
+    directory = os.getcwd()  # 현재 작업 경로
+    files = os.listdir(directory)  # 디렉터리 내의 파일 목록 가져오기
+    
+    for i, file in enumerate(files, start=1):
+        if file.startswith("_"):
+            if file.endswith(".ipynb"):  # .ipynb 확장자인 파일에 대해서만 작업 수행
+                new_name = f"{i:02d}_{file}"  # 변경될 이름 생성
+                os.rename(os.path.join(directory, file), os.path.join(directory, new_name))
+                print(f"Renamed file: {file} -> {new_name}")
+
+rename_files()
